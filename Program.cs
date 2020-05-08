25e4c3a8844a0c1c27a6ccaaeb3f5eb713661422
Christopher <CHuber42.work@gmail.com>
2020-05-08 16:49:14 -0700

    add grab-bag minigame with luck messages

--- Program.cs
+++ Program.cs
@@ -10,7 +10,24 @@ namespace Bakery
     {
       
         
-      Console.WriteLine("Welcome to Bob's Bodacious Bagel Bakery and Breadery\nWe bake 'em, you buy 'em!\nHow many loaves of bread do you want today?");
+      Console.WriteLine("Welcome to Bob's Bodacious Bagel Bakery and Breadery\nWe bake 'em, you buy 'em!");
+      Prompt1:
+      Console.WriteLine("Today we have 'Grab Bags'; for $50 get a random (between 0-20) number of loaves of bread and pastries. Want one of those, or shop normally? ([R]andom or [N]ormal) ");
+      switch (Console.ReadLine().ToLower())
+      {
+        case "n":
+          break;
+        case "r":
+          randomGoodies();
+          Environment.Exit(0);
+          break;
+        default:
+          goto Prompt1;
+
+      }
+
+      
+      Console.WriteLine("How many loaves of bread do you want today?");
       Item.Bread bread = new Item.Bread(validateIntInput());
      
       Console.WriteLine("Awesome. Now tell me how many Bagels you would like.");
@@ -100,6 +117,42 @@ namespace Bakery
       {
         return false;
       }
+    }
+
+    public static void randomGoodies()
+    {
+      string luckLevel = "";
+      double loaves = new Random().Next(0, 20);
+      double pastries = new Random().Next(0, 20);
+      double totalValue = Math.Round((loaves * 3.33) + (pastries * 1.66), 2);
+      Console.WriteLine($"You got {loaves} loaves of bread and {pastries} pastries. Their average value is ${totalValue}.");
+      switch(totalValue)
+      {
+        case double n when n > 90 :
+          luckLevel = "WOW! SO LUCKY!";
+          break;
+        case double n when n > 75 :
+          luckLevel = "Hey, that's pretty lucky!";
+          break;
+        case double n when n > 55 :
+          luckLevel = "That's a little lucky.";
+          break;
+        case double n when n >= 45 :
+          luckLevel = "That's average luck.";
+          break;
+        case double n when n < 15 :
+          luckLevel = "HOLY! THAT'S SO UNLUCKY!";
+          break;
+        case double n when n < 30 :
+          luckLevel = "Oof, that's pretty unlucky!";
+          break;
+        case double n when n < 45 :
+          luckLevel = "Dang, that's a little unlucky.";
+          break;
+      }
+ 
+      Console.WriteLine(luckLevel + ".. but thanks for stopping in! Come again soon!");
+      Environment.Exit(0);
     }
   }
 }
