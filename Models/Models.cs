07b67308010d8cbd4c2469a163a5c86129646629
Christopher <CHuber42.work@gmail.com>
2020-05-08 11:46:50 -0700

    fix Count override by setting to private; also add branches to offer to round up order if it makes sense to. Lunchtime

--- Models/Models.cs
+++ Models/Models.cs
@@ -1,25 +1,33 @@
 using System;
 
-
 namespace Bakery.Models
 {
 
   public class Item
   {
-    public static int Count {get; set;}
+    private int Count {get; set;}
     public class Bread : Item
     {
       public Bread(int count)
       {
-        Count = count;
+      Count = count;
       }
-      
+    
       public int Cost()
       {
+        recount:
         int breadSets = (Count / 3) * 10;
         int breadExtras = (Count % 3) * 5;
-        Console.WriteLine(breadSets);
-        Console.WriteLine(breadExtras);
+        Console.WriteLine($"That's {Count/3} buy-2-get-1-free deals on the bread at $10 per, subtotaling ${breadSets}, and {Count%3} unmatched loaves at $5 each subtotaling ${breadExtras}.");
+        if (Count%3 == 2)
+        {
+          Console.WriteLine("You know if you grab another loaf of bread it's free, right? Since you have 2 individual loaves here. Want to go grab one? Y/N");
+          if (Console.ReadLine().ToLower() == "y")
+          {
+            Count += 1;
+            goto recount;
+          }
+        }
 
         return breadSets + breadExtras;   
       }
@@ -34,10 +42,19 @@ namespace Bakery.Models
       }
       public int Cost()
       {
-        int pastrySets = (Count / 3);
-        int pastryExtras = (Count % 3);
-        Console.WriteLine(pastrySets);
-        Console.WriteLine(pastryExtras);
+        recount:
+        int pastrySets = (Count / 3) * 5;
+        int pastryExtras = (Count % 3) * 2;
+        Console.WriteLine($"That's {Count/3} sets-of-3 pastries at $5 per set subtotaling ${pastrySets}, and {Count%3} pastries at $2 each subtotaling ${pastryExtras}.");
+         if (Count%3 == 2)
+        {
+          Console.WriteLine("Oh, also: if you grab another pastry it's only another $1. Want to round up? Y/N");
+          if (Console.ReadLine().ToLower() == "y")
+          {
+            Count += 1;
+            goto recount;
+          }
+        }
 
         return pastrySets + pastryExtras;
       }
--- Program.cs
+++ Program.cs
@@ -9,12 +9,12 @@ namespace Bakery
     {
       Console.WriteLine("Welcome to Bob's Bodacious Bagel Bakery and Breadery\nWe bake 'em, you buy 'em!\nHow many loaves of bread do you want today?");
       Item.Bread bread = new Item.Bread(int.Parse(Console.ReadLine()));
-      Console.WriteLine(bread.Cost());
      
-      // Console.WriteLine("Awesome. Now tell me how many Bagels you would like.");
-      // Item.Pastry pastry = new Item.Pastry(int.Parse(Console.ReadLine()));
+      Console.WriteLine("Awesome. Now tell me how many Bagels you would like.");
+      Item.Pastry pastry = new Item.Pastry(int.Parse(Console.ReadLine()));
+      
 
-      // Console.WriteLine($"Ok, that'll be {bread.Cost() + pastry.Cost()} please.");
+      Console.WriteLine($"Your grand total is: ${bread.Cost() + pastry.Cost()}.");
 
    
     } 
